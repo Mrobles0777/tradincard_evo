80e12fd3c1221f738fc069b4aab292bca1ac9c57
User Name <user@example.com>
2026-03-14 12:48:56 -0300

    security: stop tracking environment.ts to prevent credential leakage

--- frontend/src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  supabaseUrl: 'https://rsjmnioucczyzwhiyyqu.supabase.co',
-  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJzam1uaW91Y2N6eXp3aGl5eXF1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMzOTc4NjYsImV4cCI6MjA4ODk3Mzg2Nn0.z0DSSIZa7VQwmUl4mVSsud3DgZNhtVKzHCDeRPcO7Tg'
-};
